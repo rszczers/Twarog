ba846019ec76a026cb5f3149e6f945c9a162c72e
Edyta Pawlak <edyta.pawlakk@gmail.com>
2019-12-12 16:25:26 +0100

    Some format

--- twarog-web/Update.hs
+++ twarog-web/Update.hs
@@ -11,18 +11,26 @@ module Update (updateModel) where
   
   -- | Updates model, optionally introduces side effects
   updateModel :: Msg -> Model -> Effect Msg Model
-  updateModel (Name n) m = noEff $ m & character . characterName .~ (Just $ fromMisoString n)
+  updateModel (Name n) m = 
+    noEff $ m & character . characterName .~ (Just $ fromMisoString n)
   updateModel NoOp m = noEff m
-  updateModel (RaceChecked r (Checked True)) m = noEff $ m & character . characterRace .~ r
-  updateModel (TalentChecked t (Checked True)) m =  let currTalents = fromMaybe [] $ m ^. character . characterTalent
-                                                    in
-                                                        noEff (if Prelude.length currTalents < maxTalents
-                                                          then m & character . characterTalent .~ Just (currTalents ++ [ t ])
-                                                          else m )
-  updateModel (TalentChecked r (Checked False)) m = let currTalents = fromMaybe [] $ m ^. character . characterTalent
-                                                    in
-                                                    noEff $ m & character . characterTalent .~
-                                                      (Just $ Prelude.filter (/= r) currTalents)
+  updateModel (RaceChecked r (Checked True)) m = 
+    noEff $ m & character . characterRace .~ r
+  updateModel (TalentChecked t (Checked True)) m =  
+    let 
+      currTalents = fromMaybe [] $ m ^. character . characterTalent
+    in
+      noEff ( 
+        if Prelude.length currTalents < maxTalents
+        then m & character . characterTalent .~ Just (currTalents ++ [ t ])
+        else m 
+        )
+  updateModel (TalentChecked r (Checked False)) m = 
+    let 
+      currTalents = fromMaybe [] $ m ^. character . characterTalent
+    in
+      noEff $ m & character . characterTalent .~
+        (Just $ Prelude.filter (/= r) currTalents)
   updateModel (ChangeStage s) m = noEff $ m & currentStage .~ s
   updateModel SayHelloWorld m = m <# do
     liftIO (putStrLn "Hello World") >> pure NoOp
