447c32e54281eff67911575318a2bfc54fe3110f
Rafał Szczerski <rafal.szczerski@gmail.com>
2019-12-14 13:43:08 +0100

    Added lunar calendar

--- src/Twarog/Backend/Calendar.hs
+++ src/Twarog/Backend/Calendar.hs
@@ -1,5 +1,6 @@
 module Twarog.Backend.Calendar
   ( Month (..)
+  , Day (..)
   , Season (..)
   , month
   , monthGod
@@ -27,6 +28,15 @@ data Month = Valaskjolf
            | Ydalir
            deriving (Ord, Eq, Show, Enum)
 
+data Day where
+  CommonDay :: Int -> Day
+  NewYearsDay :: Day
+  deriving (Eq, Show) 
+
+instance Bounded Day where
+  minBound = CommonDay 1
+  maxBound = CommonDay 28
+
 data Season = NewYear
             | Winter
             | Spring
@@ -34,11 +44,22 @@ data Season = NewYear
             | Autumn
             deriving (Eq, Show, Enum)
 
+data Birthday =
+  Birthday { birthdayDay :: Day
+           , birthdayMonth :: Month
+           } deriving (Eq, Show)
+
+-- | Get list of months.
+months :: [Month]
+months = enumFrom (toEnum 0)
+
+-- | Convert Int to @Month@.
 month :: Int -> Month
 month x = if x > 0 
           then toEnum $ x - 1
           else error "No such month"
 
+-- | Get month according to month name.
 monthSeason :: Month -> Season
 monthSeason = \case
   Valaskjolf  -> Winter
@@ -55,6 +76,7 @@ monthSeason = \case
   Thrymheimr  -> Autumn
   Ydalir      -> Autumn
 
+-- | Get list of divine's birthdays for given month.
 monthGod :: Month -> [God]
 monthGod = \case
   Valaskjolf  -> [Vali]
@@ -71,10 +93,12 @@ monthGod = \case
   Thrymheimr  -> [Skadi, Mani]
   Ydalir      -> [Hodr]
 
-seasonAttrMod :: Season -> Attributes -> Attributes
-seasonAttrMod season = case season of
+-- | Character attribute modifiers according to season of birth.
+seasonAttrMod   :: Season -> Attributes -> Attributes
+seasonAttrMod   season = case season of
   Winter  -> int +~ 1
   Spring  -> str +~ 1
   Summer  -> wil +~ 1
   Autumn  -> con +~ 1
   NewYear -> cha +~ 1
+                  
